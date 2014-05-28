c1c896466fc17a73f791c8cc946e56aed54fb004
nestofsaws <zims@nestofsaws.com>
2014-05-28 15:23:06 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Bookstore Manager</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <div id="page">
+    <header>
+        <h1>Bookstore Manager</h1>
+    </header>
+    <div id="main">
