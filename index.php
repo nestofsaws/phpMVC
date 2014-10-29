b882609c64293cbc5a3f709fd7f0666830da02b3
Brian Zimorowicz <zims@nestofsaws.com>
2014-10-29 09:18:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,36 +3,94 @@ date_default_timezone_set('America/New_York');
 require_once('databaseLibrary.php');
 $db = null;
 $bookCount = 0;
+$errorMessage = "";
+$reviewCount =0;
 
 connectToDatabase();
 
-if ( empty($_POST) || isset($_POST['list_books']) ) { 
+
+if (isset($_POST['return'])) { 
+   header('Location: .');  }
+
+if (((empty($_POST)) && (empty($_GET))) || isset($_POST['list_books'])) { 
    $statement = getListOfAllBooks();
    if ($bookCount > 0) 
       include('listBooksView.php');
+}
+
+else if (isset($_POST['search_books'])) {  
+   $searchAttribute = $_POST['attribute'];
+   $searchTerm = $_POST['searchTerm'];
+   $statement = searchBooks($searchAttribute, $searchTerm);
+   include('listBooksView.php');
+}
+
+else if (isset($_POST['add_new_book'])) { 
+   include('addBookView.php');   
+}
+else if (isset($_POST['save_new_book'])) { 
+   if (addBook() ) {
+      $isbn = $_POST['isbn'];
+      $res = getBook($isbn);
+      if ($res)
+         include('viewBookView.php');  
+      else
+         echo "Could not find book for ISBN " . $isbn;  
+   }
    else
-      echo "No results found.";
+      include('addBookView.php');   //refresh page with $errorMessage 
+}
+else if (isset($_POST['delete_book'])) { 
+   $isbn = $_POST['isbn'];
+   if (deleteBook($isbn) ) {
+      header('Location: .');   // redirect request
+   }
+   else
+      include('viewBookView.php');   //refresh page with $errorMessage 
+}
+else if (isset($_POST['update_book'])) { 
+   $isbn = $_POST['isbn'];
+   $res = getBook($isbn);
+   if ($res)
+      include('updateBookView.php');
+   else
+      echo "Could not find book for ISBN " . $isbn;      
+}
+else if (isset($_POST['save_book_updates'])) {
+   $isbn = $_POST['isbn'];
+   updateBook($isbn);
+   $res = getBook($isbn);
+   if ($res)
+      include('viewBookView.php');  
+   else
+      echo "Could not find book for ISBN " . $isbn;  
+}
+else if (isset($_POST['add_review'])) { 
+   $isbn = $_POST['isbn'];
+   $res = getBook($isbn);
+   if ($res)
+      include('addReviewView.php');
+   else
+      echo "Could not find book for ISBN " . $isbn;      
+}
+else if (isset($_POST['save_review'])) {
+   $isbn = $_POST['isbn'];
+   saveReview($isbn);
+   $res = getBook($isbn);
+   if ($res)
+      include('viewBookView.php');  
+   else
+      echo "Could not find book for ISBN " . $isbn;  
 }
 
-else if ( isset($_POST['search_books'])) {
-	$sa = $_POST['attribute'];
-	$st = $_POST['searchTerm'];
-	$statement = searchBooks($sa, $st);
-	include('listBooksView.php');
-
-}
-
-else if ( isset($_POST['add_book'])) {
-	include('addBookView.php');
-
-}
-
-else if ( isset($_POST['save_book'])) {
-	print_r($_POST);
-	addBook();
-	$statement = getListOfAllBooks();
-	include('listBooksView.php');
-
+else if(isset($_GET['isbn'])) {
+   $isbn = $_GET['isbn'];
+   $res = getBook($isbn);
+   
+   if ($res)
+      include('viewBookView.php');  
+   else
+      echo "Could not find book for ISBN $isbn";
 }
 
 ?>
